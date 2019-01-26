b4fbe10924cdfa80ac22bf4f7f966113049d1f74
Isabelle Bouchard <isabelle@elementai.com>
2019-01-26 13:29:07 -0500

    partie finito pepito

--- TP1/partie1.py
+++ TP1/partie1.py
@@ -25,24 +25,26 @@ def main():
     holmes = np.array([[1, 0], [0.1, 0.9], [0, 1], [0, 1]]).reshape(2, 2, 1, 2)
     print('Pr(Holmes|Pluie, Arroseur)={}\n'.format(np.squeeze(holmes)))
 
+    prob_conjointe = pluie * arroseur * holmes * watson
+
     # A. Pr(W=1)
-    A = (watson * pluie)[:, :, 1, :].sum()
+    A = prob_conjointe[:, :, 1, :].sum()
     print('A. Pr(W=1) = {}'.format(A))
 
     # B. Pr(W=1|H=1)
-    B = (pluie * arroseur * holmes * watson)[:, :, 1, 1].sum()/(holmes * pluie * arroseur)[:, :, :, 1].sum()
+    B = prob_conjointe[:, :, 1, 1].sum()/prob_conjointe[:, :, :, 1].sum()
     print('B. Pr(W=1|H=1) = {}'.format(B))
 
-    # C. Pr(W=1|H=1,A=0)
-    C = (pluie * arroseur * holmes * watson)[:, 0, 1, 1].sum()/(holmes * pluie * arroseur)[0, :, :, 1].sum()
+    # C. Pr(W=1|H=1, A=0)
+    C = prob_conjointe[:, 0, 1, 1].sum()/prob_conjointe[0, :, :, 1].sum()
     print('C. Pr(W=1|H=1,A=0) = {}'.format(C))
 
     # D. Pr(W=1|A=0)
-    D = (pluie * arroseur * holmes * watson)[:, 0, :, 1].sum()/(arroseur)[:, 0, :, :].sum()
+    D = prob_conjointe[:, 0, 1, :].sum()/prob_conjointe[:, 0, :, :].sum()
     print('C. Pr(W=1|A=0) = {}'.format(D))
 
     # E. Pr(W=1|P=1)
-    E = (pluie * arroseur * holmes * watson)[1, :, :, 1].sum()/(pluie)[1, :, :, :].sum()
+    E = prob_conjointe[1, :, 1, :].sum()/prob_conjointe[1, :, :, :].sum()
     print('C. Pr(W=1|P=1) = {}'.format(E))
 
 
